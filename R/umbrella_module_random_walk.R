63bcb49a71de7668d49b401751fa7d24beb4f583
William Whinn <w.willis.1986@gmail.com>
2019-07-22 15:15:57 +0100

    Add basic argument validation

--- R/umbrella_module_random_walk.R
+++ R/umbrella_module_random_walk.R
@@ -3,7 +3,7 @@
 # MSc Data Science (Full Time)
 # PROM02 Computing Master's Project
 
-# File Name: umbrella_module_random_walk..R
+# File Name: umbrella_module_random_walk.R
 # File Author: William Whinn
 
 ##############
@@ -16,11 +16,13 @@
 # Documentation #
 #################
 
-# NOTE: Current implementation will be based on logic from 'igraph' 'random_walk()' C++ code.
+# NOTES:
+# - Current implementation will be based on logic from 'igraph' 'random_walk()' C++ code.
+# - Code has been formatted to demonstrate a C-style development process.
 
 # Function: RandomWalk
 # Parameters:
-# - data
+# - dataset
 # - start_node
 # - steps
 # - random_seed ('TRUE', 'FALSE')
@@ -28,26 +30,39 @@
 # - stuck ('stop_walk', 'raise_error')
 
 #############
-# Variables #
-#############
-
-#############
 # Functions #
 #############
 
-RandomWalk <- function(
-  dataset,
-  start_node,
-  steps,
-  random_seed = c(TRUE, FALSE),
-  mode = c('in', 'out', 'all'),
-  stuck = c('stop_walk', 'raise_error')
-) {
-  print('') # Placeholder
-}
+RandomWalk <-
+  function(dataset = 'placeholder',  # This will be removed for production.
+           start_node,
+           steps,
+           random_seed = c(TRUE, FALSE),
+           mode = c('in', 'out', 'all'),
+           stuck = c('stop_walk', 'raise_error')) {
+    # Argument Parsing: dataset
+    # - This will fail in production if data are not applied.
+    # - A placeholder is currently present to allow execution of other logic.
 
-#############
-# Kickstart #
-#############
+    # Argument Parsing: random_seed
+    if (isTRUE(random_seed))
+    {
+      print('NOTE: Applying a unique psuedo-random seed.')
+      ApplyRandomSeed()
+    }
+    else if (isFALSE(random_seed))
+    {
+      print('NOTE: Proceeding with default pseudo-random seed.')
+    }
+    else if (missing(random_seed))
+    {
+      print('NOTE: Proceeding with default pseudo-random seed.')
+    }
+    else
+    {
+      print('ERROR: An unknown error occurred.')
+      return()
+    }
+  }
 
 # End of File.
