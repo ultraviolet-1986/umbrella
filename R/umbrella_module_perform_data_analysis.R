a91bd30c28d1843d695b6c1b876bde041995dc22
William Whinn <w.willis.1986@gmail.com>
2019-07-29 10:57:58 +0100

    Clean code.

--- R/umbrella_module_perform_data_analysis.R
+++ R/umbrella_module_perform_data_analysis.R
@@ -18,22 +18,36 @@
 
 PerformDataAnalysis <- function(dataset)
 {
+  ###################
+  # Data Validation #
+  ###################
+
   if (missing(dataset))
   {
-    # Failure: Data have not been provided. Terminate analysis.
+    # FAILURE: Data have not been provided. Terminate analysis.
     print(paste("ERROR: Argument 'dataset' has not been defined. Terminating",
                 "Data Analysis."))
     return()
   }
   else if (typeof(dataset) == 'list')
   {
-    # Success: Data exists and is in the correct format.
+    # SUCCESS: Data exists and is in the correct format.
     print("NOTE: Data is of type 'List'. Performing Data Analysis.")
+
+    # NOTES:
+    # - Continue outside of IF statement.
+    # - Error conditions will terminate data analysis.
   }
   else
   {
-    # Failure: Catch unknown error.
+    # FAILURE: Catch unknown error.
     print("ERROR: An unknown error occurred. Terminating Data Analysis.")
     return()
   }
+
+  #################
+  # Data Analysis #
+  #################
+
+  print("NOTE: Validation Complete. Listing results.")
 }
