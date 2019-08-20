7efe694bdcb4bff05e598e03a47406bbea78bf35
William Whinn <w.willis.1986@gmail.com>
2019-08-20 15:14:57 +0100

    Correct randomly generated network

--- R/umbrella_module_generate_random_network_file.R
+++ R/umbrella_module_generate_random_network_file.R
@@ -15,9 +15,8 @@ GenerateRandomNetwork <- function(nodes = 25, to_file = c(TRUE, FALSE))
   # Prevent re-writing the file name.
   umbrella_random_network_file <- 'umbrella_random_network.txt'
 
-  # Create an undirected graph to export as both data and a text file.
-  umbrella_random_network <- sample_pa(nodes, power = 2, directed = FALSE)
-  # umbrella_random_network <- erdos.renyi.game(nodes, .33)
+  # Create an 'Edros Renyi' graph to export as both data and a text file.
+  umbrella_random_network <- erdos.renyi.game(nodes, .33)
 
   # Export the data as a text file within the user's current working directory
   # and overwrite any existing data.
