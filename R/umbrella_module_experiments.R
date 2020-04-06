35e8902e1ed34034b05fb2107c182049fb8371f4
William Whinn <w.willis.1986@gmail.com>
2020-04-06 10:51:57 +0100

    Include GPLv3 notice in R scripts

--- R/umbrella_module_experiments.R
+++ R/umbrella_module_experiments.R
@@ -1,10 +1,24 @@
 #!/usr/bin/env Rscript
 
-# MSc Data Science (Full Time)
-# PROM02 Computing Master's Project
+##########
+# Notice #
+##########
 
-# File Name: umbrella_module_experiments.R
-# File Author: William Whinn
+# Umbrella: A Biased Generic Random Walk Algorithm for Community Detection
+# Copyright (C) 2020 William Willis Whinn
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ##############
 # References #
